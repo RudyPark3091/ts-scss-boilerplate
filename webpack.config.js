8b72d8fc69b177159972529a469eac69abfa3397
rudy3091 <grayblack313@gmail.com>
2021-04-22 20:48:46 +0900

    Add path alias

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
 	entry: "./src/index.ts",
 	resolve: {
 		extensions: [".ts", ".js", ".css", ".scss"],
+		alias: {
+			"@type": path.resolve(__dirname, "src/types"),
+		},
 	},
 	watch: true,
 	watchOptions: {
